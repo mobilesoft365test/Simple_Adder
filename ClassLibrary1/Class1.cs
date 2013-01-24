f955d9fa1b821a57c2288c78fad911fa635f7a19
mobilesoft365test <info@mobilesoft365.com>
2013-01-24 13:39:59 +0100

    ыыы

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NUnit.Framework;
+using NUnit.Extensions.Forms;
+using SAdder;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+
+
+        [Test]
+        public void MethodTest()
+        {
+            Assert.IsTrue(SAdder.MainForm.Add(112, 333)==445);
+        }
+    }
+}
