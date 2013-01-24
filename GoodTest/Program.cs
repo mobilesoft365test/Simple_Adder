e96e5905272b9fd1880c7af3c290f067206409b1
mobilesoft365test <info@mobilesoft365.com>
2013-01-24 13:00:48 +0100

    goodtest

---
+++ GoodTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using NUnit.Framework;
+using SAdder;
+
+namespace GoodTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    [TestFixture]
+    class TestClass
+    {
+        [Test]
+        public void MethodTest()
+        {
+            Assert.IsTrue(SAdder.MainForm.Add(2,3)==5);
+        }
+    }
+}
